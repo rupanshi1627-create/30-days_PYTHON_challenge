1b73618b52dea2c0b2d7ec7ce3eb627bdfcf80f2
Rupanshi <rupanshi1627@gmail.com>
2026-09-07 21:27:30 +0530

    .

--- dec target practice/1.2sum.py
+++ dec target practice/1.2sum.py
@@ -1,63 +1,18 @@
+#TWO SUM 
 # Test 1:
-nums = [2, 7, 11, 15]
-target = 9
-# Expected: [0, 1]
-left=0
-right=len(nums)-1
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 10
+left = 0
+right = len(nums) - 1
 
-while left<right:
-    current_sum=nums[left]+nums[right]
-    if current_sum==target:
-        print(left,right)
-    elif current_sum<target:
-        left+=1
+while left < right:
+    current_sum = nums[left] + nums[right]
+    if current_sum == target:
+        print(nums[left], nums[right])
+        break
+    elif current_sum < target:
+        left += 1
     else:
-        right-=1
-
-
-print(current_sum)
-
-
-
-
-# Test 2:
-nums = [1, 3, 4, 5, 7, 11]
-target = 9
-# Test 1:
-nums = [2, 7, 11, 15]
-target = 9
-# Expected: [0, 1]
-left=0
-right=len(nums)-1
-
-while left<right:
-    current_sum=nums[left]+nums[right]
-    if current_sum==target:
-        print(left,right)
-    elif current_sum<target:
-        left+=1
-    else:
-        right-=1
-print(current_sum)
-
-# Test 3:
-nums = [1, 2, 3, 4, 5]
-target = 9
-# Expected: [3, 4] (because nums[3]=4, nums[4]=5, and 4+5=9)
-# Test 1:
-nums = [2, 7, 11, 15]
-target = 9
-# Expected: [0, 1]
-left=0
-right=len(nums)-1
-
-while left<right:
-    current_sum=nums[left]+nums[right]
-    if current_sum==target:
-        print(left,right)
-    elif current_sum<target:
-        left+=1
-    else:
-        right-=1
-
-print(current_sum)
+        right -= 1
+else:
+    print("No two numbers found that add up to the target.")
